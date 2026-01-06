dcbc424b3e287da1070cd68c12e8896c89eda63f
Nithakm8 <nithakm8@gmail.com>
2026-01-06 23:15:34 +0530

    portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+export default function App() {
+  return (
+    <div className="font-sans scroll-smooth">
+      <Navbar />
+      {/* <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Education />
+      <Contact /> */}
+    </div>
+  );
+}
